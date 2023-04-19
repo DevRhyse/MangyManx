6ebb769e3302ecc2a9c978c90301de61792c9a0d
Rhyse <dev.rhyse@gmail.com>
2023-04-18 23:46:10 -0500

    project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+export default function App() {
+
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    </div>
+  )
+}
